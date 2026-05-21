66f8350733bd52f9a8e80e6feac3f6ee89b56fb2
Gareth Bowen <gareth@bowenwebdesign.co.nz>
2026-05-22 11:33:11 +1200

    spaces

--- vite.config.ts
+++ vite.config.ts
@@ -20,7 +20,7 @@ const copyConfigFile = viteStaticCopy({
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), copyConfigFile],
-	base: './',
+  base: './',
   define: {
     __WEB_FORMS_VERSION__: JSON.stringify(webFormsPackage.version)
   },

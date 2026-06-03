3b8f7cf80acdcfb8ef327d614e31cb347835d5da
latin-panda <66472237+latin-panda@users.noreply.github.com>
2026-06-03 22:25:17 +0700

    Take 2: does it work better with playwright.config.ts?

---
+++ playwright.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig, devices } from '@playwright/test';
+
+export default defineConfig({
+	testDir: './test',
+	forbidOnly: !!process.env.CI,
+  workers: process.env.CI ? 1 : undefined,
+	use: {
+		baseURL: 'http://localhost:5173',
+		trace: 'on-first-retry',
+	},
+	projects: [
+		{
+			name: 'chromium',
+			use: {
+				...devices['Desktop Chrome'],
+				channel: 'chrome',
+			},
+		},
+	],
+});
